0769de9bbf0311d5d1803f012014c512b3002334
Ronen Narkis <narkisr@gmail.com>
2013-09-01 00:16:31 +0300

    creating multiple drives

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,9 +1,17 @@
 group{ 'puppet': ensure  => present }
 
 node 'ubuntu.local' {
-  class{'mkfs': device => '/dev/sdb', dest => '/mnt/drive/'}
+  file{'/mnt/drive-a': ensure    => directory} ->
+  mkfs::device{'/dev/sdb':  dest => '/mnt/drive-a/'}
+
+  file{'/mnt/drive-b': ensure    => directory} ->
+  mkfs::device{'/dev/sdc':  dest => '/mnt/drive-b/'}
 }
 
 node 'centos.local' {
-  class{'mkfs': device => '/dev/sdb', dest => '/mnt/drive/'  }
+  file{'/mnt/drive-a': ensure    => directory} ->
+  mkfs::device{'/dev/sdb':  dest => '/mnt/drive-a/'}
+
+  file{'/mnt/drive-b': ensure    => directory} ->
+  mkfs::device{'/dev/sdc':  dest => '/mnt/drive-b/'}
 }
